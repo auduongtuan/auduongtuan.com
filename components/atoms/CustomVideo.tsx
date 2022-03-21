6392733b01425e6422e4187547f19fe7d42120f8
Tuan Au <audtuan@gmail.com>
2022-03-21 23:22:45 +0700

    Enhance lazy load for video

--- components/atoms/CustomVideo.tsx
+++ components/atoms/CustomVideo.tsx
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef, useImperativeHandle } from "react";
 export interface CustomVideoProps {
   poster?: string;
   src: string;
-  width?: string|number;
-  height?: string|number;
+  width?: number;
+  height?: number;
   slug: string;
   autoPlay?: boolean;
+  preload?: boolean;
   loop?: boolean;
   className?: string;
+  show?: boolean;
 }
-const CustomVideo = React.forwardRef<HTMLVideoElement, CustomVideoProps>(({poster, src, width, height, slug, autoPlay = true, loop = true, className = ''}, ref) => {
+
+const CustomVideo = React.forwardRef<HTMLVideoElement, CustomVideoProps>(({poster, src, width, height, slug, autoPlay = true, loop = true, className = '', preload = false, show = true}, ref) => {
+  const innerRef = useRef<HTMLVideoElement>(null);
+  useImperativeHandle(ref, () => innerRef.current as HTMLVideoElement);
+  useEffect(() => {
+    if ("IntersectionObserver" in window) {
+      const lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(video) {
+          console.log(video);
+          if (video.isIntersecting && video.target instanceof HTMLVideoElement && video.target.dataset.src && show) {
+            // console.log(video.target);
+            // for (const source in video.target.children) {
+            //   const videoSource = video.target.children[source] as HTMLVideoElement;
+            //   if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+            //     videoSource.src = videoSource.dataset.src;
+            //   }
+            // }
+            video.target.src = video.target.dataset.src;
+            video.target.load();
+            video.target.classList.remove("lazy");
+            lazyVideoObserver.unobserve(video.target);
+          }
+        });
+      });
+      // console.log(innerRef);
+      if (innerRef.current instanceof HTMLVideoElement) lazyVideoObserver.observe(innerRef.current);
+    }
+  }, [ref, show]);
   // return <video className='w-full h-auto' poster={poster && poster} src={require(`../../public/uploads/${slug}/${src}`)} width={width ? width : undefined} height={height ? height: undefined} loop={loop} muted={autoPlay} autoPlay={autoPlay} playsInline={autoPlay} preload="true"></video>
-  return <video ref={ref} className={`w-full h-auto ${className}`} poster={poster && poster} src={`/uploads/${slug}/${src}`} width={width ? width : undefined} height={height ? height: undefined} loop={loop} muted={autoPlay} autoPlay={autoPlay} playsInline={autoPlay} preload="true"></video>
+  return (
+
+      <video ref={innerRef} className={`w-full h-auto ${className}`} poster={poster && poster} data-src={`/uploads/${slug}/${src}`} width={width ? width : undefined} height={height ? height: undefined} loop={loop} muted={autoPlay} autoPlay={autoPlay} playsInline={autoPlay} preload={preload ? "true" : "false"}></video>
+  );
 });
 CustomVideo.displayName = 'CustomVideo';
 export default CustomVideo;
--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Button from "../atoms/Button";
 import { useInView } from 'react-intersection-observer';
 import { useAppContext } from "../../lib/context/AppContext";
@@ -11,17 +11,22 @@ import { PhotoFrame } from "../atoms/Frame";
 
 interface GifTextProps extends React.HTMLProps<HTMLAnchorElement> {
   link?: string;
-  gif?: HTMLElement|null;
+  // gif?: HTMLElement|null;
+  gifs: React.MutableRefObject<(HTMLDivElement | null)[]>;
+  gifIndex: number;
   external?: boolean;
+  setActiveGif?: Function;
 }
-const GifText = ({children, gif, link, external = false, ...rest}: GifTextProps) => {
+const GifText = ({children, gifs, gifIndex, link, external = false, setActiveGif, ...rest}: GifTextProps) => {
   const showGif = () => {
     // console.log(gif);
-    gif?.classList.add('animate-slide-in-fast');
+    gifs.current[gifIndex]?.classList.add('animate-slide-in-fast');
+    if(setActiveGif) setActiveGif(gifIndex);
   }
   const hideGif = () => {
     // console.log(gif);
-    gif?.classList.remove('animate-slide-in-fast');
+    gifs.current[gifIndex]?.classList.remove('animate-slide-in-fast');
+    if(setActiveGif) setActiveGif(null);
   }
   
   const className = "underline decoration-2 underline-offset-4 transition-all duration-200 decoration-gray-600 hover:decoration-transparent inline-block -mx-2 px-2 py-1 rounded-xl hover:bg-white/5";
@@ -49,24 +54,23 @@ export default function Header() {
     appContext && appContext.setHeaderInView && appContext.setHeaderInView(inView)    
     // console.log(entry);
   }, [inView, appContext]);
-  const gif1 = useRef<HTMLDivElement>(null);
-  const gif2 = useRef<HTMLDivElement>(null);
-  const gif3 = useRef<HTMLDivElement>(null);
+  const gifs = useRef<(HTMLDivElement | null)[]>([]);
+  const [activeGif, setActiveGif] = useState<number | null>(null);
   return (
     <header ref={ref} className="bg-custom-neutral-900 text-white w-full z-10">
       <div className="main-container p-header">
         <div className="grid grid-cols-12 gap-3">
           <div className="col-span-12 col-start-1 row-start-1 row-end-2 lg:col-span-8 lg:max-w-[50rem] z-4">
             <Headline />
-            <p className="_font-display text-xl md:text-2xl mt-6 lg:mt-9 _tracking-tight opacity-0 animate-slide-in-fast animation-delay-200">I am a <GifText gif={gif1?.current}>designer</GifText> x <GifText gif={gif2?.current}>developer</GifText> hybrid (maybe a  <GifText link="https://designtechnologist.club/book/who-is-a-design-technologist/" title="Read more about this term" gif={gif3?.current} external>design technologist</GifText>?). Everyday I try to make good products with the human at the center</p>
+            <p className="_font-display text-xl md:text-2xl mt-6 lg:mt-9 _tracking-tight opacity-0 animate-slide-in-fast animation-delay-200">I am a <GifText gifs={gifs} gifIndex={0} setActiveGif={setActiveGif}>designer</GifText> x <GifText gifs={gifs} gifIndex={1} setActiveGif={setActiveGif}>developer</GifText> hybrid (maybe a  <GifText link="https://designtechnologist.club/book/who-is-a-design-technologist/" title="Read more about this term" gifs={gifs} gifIndex={2} setActiveGif={setActiveGif} external>design technologist</GifText>?). Everyday I try to make good products with the human at the center</p>
             <Button href="/about" className="mt-10 opacity-0 animate-slide-in-fast animation-delay-300" colorful arrow>
               Get to know me
             </Button>
           </div>
           <div className="col-span-12 col-start-1 row-start-1 row-end-2 lg:col-span-4 relative z-30 pointer-events-none md:block">
-            <PhotoFrame ref={gif1} className="absolute md:top-0 opacity-0" name="lam-dau-tram-ho.gif" inverted><CustomVideo className="w-full h-auto " slug="gif" src="designer.mp4" width={480} height={264} /></PhotoFrame>
-            <PhotoFrame ref={gif2} className="absolute md:bottom-0 opacity-0" name="muon-bung-chay.gif" inverted><CustomVideo className="w-full h-auto" slug="gif" src="codes.mp4" width={480} height={270} /></PhotoFrame>
-            <PhotoFrame ref={gif3} className="absolute md:-bottom-8 opacity-0" name="unicorn-designer.gif" inverted><CustomVideo className="w-full h-auto" slug="gif" src="unicorn.mp4" width={480} height={480} /></PhotoFrame>
+            <PhotoFrame ref={el => gifs.current[0] = el} className="absolute md:top-0 opacity-0" name="lam-dau-tram-ho.gif" inverted><CustomVideo className="w-full h-auto" slug="gif" src="designer.mp4" width={480} height={264} show={activeGif == 0} /></PhotoFrame>
+            <PhotoFrame ref={el => gifs.current[1] = el} className="absolute md:bottom-0 opacity-0" name="muon-bung-chay.gif" inverted><CustomVideo className="w-full h-auto" slug="gif" src="codes.mp4" width={480} height={270} show={activeGif == 1} /></PhotoFrame>
+            <PhotoFrame ref={el => gifs.current[2] = el} className="absolute md:-bottom-8 opacity-0" name="unicorn-designer.gif" inverted><CustomVideo className="w-full h-auto" slug="gif" src="unicorn.mp4" width={480} height={480} show={activeGif == 2} /></PhotoFrame>
           </div>
         </div>
       </div>
