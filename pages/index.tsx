4a80e1a1564b13f950b46c193f2d23c1364db911
Tuan Au Duong <audtuan@gmail.com>
2023-06-25 17:43:02 +0700

    feat: optimize seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import HeadMeta from "../components/atoms/HeadMeta";
 export default function Index({ posts, projects }: HomePageProps) {
   return (
     <>
-      <HeadMeta description="This is a personal website of AU DUONG TUAN - a software designer/developer" />
+      <HeadMeta description="This is a personal website of AU DUONG TUAN - A software designer / developer / whatever who strives to make good things with the human at the center" />
       <HomePage projects={projects} posts={posts} />
     </>
   );
