c543d9ebf7c6ba2cee5e4295450193722f856f2a
Tuan Au Duong <audtuan@gmail.com>
2024-06-23 17:18:49 +0700

    feat: Content enhancement

--- components/templates/project/ProjectComponents.tsx
+++ components/templates/project/ProjectComponents.tsx
@@ -8,6 +8,7 @@ import CustomVideo, { CustomVideoProps } from "@atoms/CustomVideo";
 import InlineLink from "@atoms/InlineLink";
 import Persona, { PersonaProps } from "@atoms/Persona";
 import Figure from "@atoms/Figure";
+import Vimeo from "@atoms/Vimeo";
 
 const ProjectComponents = (slug: string) => ({
   h2: ({ children }: { children: React.ReactNode }) => (
@@ -33,26 +34,7 @@ const ProjectComponents = (slug: string) => ({
   Image: (props: CustomImageProps) => {
     return <CustomImage {...props} slug={slug} />;
   },
-  Vimeo: ({ id, ratio = 56.25 }: { id: string | number; ratio: number }) => (
-    <div
-      className="vimeo"
-      style={{ padding: `${ratio}% 0 0 0`, position: "relative" }}
-    >
-      <iframe
-        title="Video"
-        src={`https://player.vimeo.com/video/${id}?title=0&byline=0&portrait=0&sidedock=0`}
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-        }}
-        frameBorder="0"
-        allow="autoplay; fullscreen; picture-in-picture"
-      ></iframe>
-    </div>
-  ),
+  Vimeo,
   Video: (props: CustomVideoProps) => <CustomVideo {...props} slug={slug} />,
   Figure,
   Note,
