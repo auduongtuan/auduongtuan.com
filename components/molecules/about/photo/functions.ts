fc586e1f13f802596bc49f5fe475e7444e7f2c02
Tuan Au Duong <audtuan@gmail.com>
2025-05-14 21:04:09 +0700

    fix: photo image bug

--- components/molecules/about/photo/functions.ts
+++ components/molecules/about/photo/functions.ts
@@ -32,7 +32,7 @@ export function swipeAction(direction: Direction, photo: DisplayPhoto) {
       trackEvent({
         event: "swipe_reaction",
         content: "left",
-        page: "/about#photo-" + photo.id,
+        page: "/about#photo-" + photo.image,
       });
       break;
     case Direction.RIGHT:
@@ -42,7 +42,7 @@ export function swipeAction(direction: Direction, photo: DisplayPhoto) {
       trackEvent({
         event: "swipe_reaction",
         content: "right",
-        page: "/about#photo-" + photo.id,
+        page: "/about#photo-" + photo.image,
       });
       break;
     case Direction.TOP:
@@ -52,7 +52,7 @@ export function swipeAction(direction: Direction, photo: DisplayPhoto) {
       trackEvent({
         event: "swipe_reaction",
         content: "top",
-        page: "/about#photo-" + photo.id,
+        page: "/about#photo-" + photo.image,
       });
       break;
   }
