3844fff2685d77a6cf280e9eed813dfdf07e99d2
Tuan Au Duong <tuan.au@bioturing.com>
2026-02-14 22:19:29 +0700

    fix: reset first/last margins in MiniPostCard to hide unwanted scrollbar

--- components/molecules/post/MiniPostCard.tsx
+++ components/molecules/post/MiniPostCard.tsx
@@ -1,9 +1,9 @@
+import Tag from "@atoms/Tag";
 import { Post } from "@lib/notion";
+import { formatPostDate } from "@lib/utils/format";
+import clsx from "clsx";
 import Link from "next/link";
 import { FiLock } from "react-icons/fi";
-import Tag from "@atoms/Tag";
-import clsx from "clsx";
-import { formatPostDate } from "@lib/utils/format";
 
 type MiniPostCardProps = {
   post: Post;
@@ -17,7 +17,7 @@ const MiniPostCard = ({ post, className = "" }: MiniPostCardProps) => {
       href={`/blog/${post.slug}`}
       key={post.slug}
       className={clsx(
-        "ease -m-4 flex rounded-lg p-4 transition-all duration-100 hover:bg-gray-100",
+        "ease -m-4 flex rounded-lg p-4 transition-all duration-100 group-first/post:mt-0 group-last/post:mb-0 hover:bg-gray-100",
         className,
       )}
     >
@@ -43,9 +43,7 @@ const MiniPostCard = ({ post, className = "" }: MiniPostCardProps) => {
           ))}
         </div>
         {formattedDate && (
-          <p className={clsx("muted-text mt-2")}>
-            Posted on {formattedDate}
-          </p>
+          <p className={clsx("muted-text mt-2")}>Posted on {formattedDate}</p>
         )}
       </div>
     </Link>
